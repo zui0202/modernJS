6ed6bffcec924d3427dc2a270cfbce0a130eeaa5
zui0202 <zui0202@naver.com>
2022-08-02 16:48:40 +0900

    const & immutable array

--- scope.js
+++ scope.js
@@ -1,7 +1,12 @@
 function home() {
-  const homename = "my house";
-  homename = "your house";
-  console.log(homename);
+  const list = ["apple", "orange", "watermelon"];
+  list.push("banana");
+  console.log(list);
+  // const를 사용하더라도 배열과 오브젝트의 값을 변경하는 것 가능함
 }
 
-home();
+//immutable array
+const list = ["apple", "orange", "watermelon"];
+list2 = [].concat(list, "banana");
+console.log(list, list2);
+console.log(list === list2);
