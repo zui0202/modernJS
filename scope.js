03149dcab67146e8b05d5b94643140886ea1dd2a
zui0202 <zui0202@naver.com>
2022-08-02 16:52:53 +0900

    string methods

--- scope.js
+++ scope.js
@@ -1,12 +1,5 @@
-function home() {
-  const list = ["apple", "orange", "watermelon"];
-  list.push("banana");
-  console.log(list);
-  // const를 사용하더라도 배열과 오브젝트의 값을 변경하는 것 가능함
-}
+let str = "hello world ^^ ~~";
+let matchstr = "hello";
 
-//immutable array
-const list = ["apple", "orange", "watermelon"];
-list2 = [].concat(list, "banana");
-console.log(list, list2);
-console.log(list === list2);
+console.log(str.startsWith(matchstr));
+console.log(str.includes(matchstr));
