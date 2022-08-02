1f861013350181fe6c27ce6daf6465908bc55bae
zui0202 <zui0202@naver.com>
2022-08-02 17:01:29 +0900

    spread operator 1

--- array.js
+++ array.js
@@ -1,12 +1,3 @@
-var data = [1, 2, undefined, NaN, null, ""];
-
-Array.prototype.getIndex = function () {};
-
-for (let val of data) {
-  console.log(val);
-}
-
-var str = "hello world!";
-for (let val of str) {
-  console.log(val);
-}
+let pre = ["apple", "orange", 100];
+let newData = [...pre];
+console.log(pre === newData);
