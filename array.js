b92e82df4b3f8fe6993ecb2298714e22f245bcf3
zui0202 <zui0202@naver.com>
2022-08-02 17:06:47 +0900

    spread operator 2

--- array.js
+++ array.js
@@ -1,3 +1,9 @@
-let pre = ["apple", "orange", 100];
-let newData = [...pre];
-console.log(pre === newData);
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+let pre = [100, 200, 300];
+
+console.log(sum.apply(null, pre));
+
+console.log(sum(...pre));
